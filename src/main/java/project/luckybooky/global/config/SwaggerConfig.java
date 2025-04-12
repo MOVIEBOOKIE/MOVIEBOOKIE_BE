d1657c9005482724f6852bcd294b506e0cb5c6f4
m020202 <m020202@naver.com>
2025-04-12 20:09:15 +0900

    [DOCS] #1: 스웨거 Server 정보 변경

--- src/main/java/project/luckybooky/global/config/SwaggerConfig.java
+++ src/main/java/project/luckybooky/global/config/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package project.luckybooky.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "MOVIEBOOKIE API 명세서",
+                description = "MOVIEBOOKIE API 명세서",
+                version = "v1"),
+        servers = @Server(url = "/", description = "Default Server URL"))// Server 정보 추가)
 @Configuration
 public class SwaggerConfig {
 
