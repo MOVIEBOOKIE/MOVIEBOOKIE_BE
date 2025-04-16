e027ca3707bff4c255d359bfedbd19846d079eb4
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-04-16 17:33:47 +0900

    [FEAT] #5: swagger Import 충돌 해결

--- src/main/java/project/luckybooky/global/config/SwaggerConfig.java
+++ src/main/java/project/luckybooky/global/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -34,7 +33,6 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(apiInfo())
-                .addServersItem(new Server().url("/"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
