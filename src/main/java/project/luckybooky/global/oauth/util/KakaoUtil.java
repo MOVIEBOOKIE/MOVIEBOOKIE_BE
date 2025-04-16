9c156e290b2c7612c14a48d744b0970f5b696b53
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-04-16 17:25:45 +0900

    [FEAT] #5: 도메인 주소 추가

--- src/main/java/project/luckybooky/global/oauth/util/KakaoUtil.java
+++ src/main/java/project/luckybooky/global/oauth/util/KakaoUtil.java
@@ -29,7 +29,7 @@ public class KakaoUtil {
     private final ObjectMapper objectMapper;
     private final Set<String> allowedRedirectUris = Set.of(
             "http://localhost:3000/login/kakao",
-            "http://api-movie-bookie.shop/"
+            "https://api-movie-bookie.shop/"
     );
 
 
