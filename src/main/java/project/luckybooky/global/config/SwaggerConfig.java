5429043e925d1aa43a0ecc215d5d78dc693d4b88
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-04-16 17:08:40 +0900

    [FEAT] #5: spring 버전 수정
    swagger 동작 오류

--- src/main/java/project/luckybooky/global/config/SwaggerConfig.java
+++ src/main/java/project/luckybooky/global/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package project.luckybooky.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +14,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        Components components = new Components()
+                .addSecuritySchemes(jwt, securityScheme);
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
+
 
     private Info apiInfo() {
         return new Info()
