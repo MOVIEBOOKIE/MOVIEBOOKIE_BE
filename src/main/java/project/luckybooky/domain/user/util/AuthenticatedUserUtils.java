5dc25953d9289235ab2867b60f14e1203c6d823f
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-04-18 15:22:35 +0900

    [REFACTOR] #5: util 패키지 생성

--- src/main/java/project/luckybooky/domain/user/util/AuthenticatedUserUtils.java
+++ src/main/java/project/luckybooky/domain/user/util/AuthenticatedUserUtils.java
@@ -1,4 +1,4 @@
-package project.luckybooky.domain.user;
+package project.luckybooky.domain.user.util;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
