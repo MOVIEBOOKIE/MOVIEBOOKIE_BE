0a4bbeee3a27176403d96715fd19f782fe2f656a
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-02 18:42:19 +0900

    [INIT] #16: 유저 타입 결과 라벨 조회 가능하도록 설정

--- src/main/java/project/luckybooky/domain/user/entity/UserType.java
+++ src/main/java/project/luckybooky/domain/user/entity/UserType.java
@@ -4,33 +4,34 @@ import lombok.Getter;
 
 @Getter
 public enum UserType {
-    // 🎬 영화
-    MOVIE_DETAIL_COLLECTOR("🎞 디테일 수집형 영화 덕후러",    ContentCategory.MOVIE,   GroupType.A),
-    MOVIE_HOTFLIX("🍿 핫플릭스만 골라보는 감각 감상러",      ContentCategory.MOVIE,   GroupType.B),
 
-    // 📺 드라마
-    DRAMA_SERIES_IMMERSION("💜 대사에 숨멎하는 ‘서사 몰입러’", ContentCategory.DRAMA,   GroupType.A),
-    DRAMA_HIGHLIGHT_TRACKER("💡 명장면 추적러",             ContentCategory.DRAMA,   GroupType.B),
+    /*  🎬 영화  */
+    MOVIE_DETAIL_COLLECTOR (ContentCategory.MOVIE,   GroupType.A, "🎞 디테일 수집형 영화 덕후러"),
+    MOVIE_TRENDY_VIEWER    (ContentCategory.MOVIE,   GroupType.B, "🍿 핫플릭스만 골라보는 감각 감상러"),
 
-    // 😂 예능
-    VARIETY_LEGEND_HUNTER("🤣 레전드 찾아 삼만리 예능러",    ContentCategory.VARIETY, GroupType.A),
-    VARIETY_MEME_HUNTER("⚡ 밈 사냥 헌터",                   ContentCategory.VARIETY, GroupType.B),
+    /*  📺 드라마  */
+    DRAMA_STORY_IMMERSER   (ContentCategory.DRAMA,   GroupType.A, "💜 대사에 숨멎하는 ‘서사 몰입러’"),
+    DRAMA_HIGHLIGHT_HUNTER (ContentCategory.DRAMA,   GroupType.B, "💡 레전드 회차 ‘명장면 추적러’"),
 
-    // 🏀 스포츠
-    SPORTS_FULL_ENGAGER("🏃 승부에 인생 건 몰입 응원러",    ContentCategory.SPORTS,  GroupType.A),
-    SPORTS_HIGHLIGHT_RABBIT("🧊 알잘딱깔센 스포츠러",       ContentCategory.SPORTS,  GroupType.B),
+    /*  😂 예능  */
+    VARIETY_DETAIL_ANALYST (ContentCategory.VARIETY, GroupType.A, "🤣 레전드 찾아 삼만리 예능러"),
+    VARIETY_MEME_COLLECTOR (ContentCategory.VARIETY, GroupType.B, "⚡ 짤줍 맛집 밈 사냥 헌터"),
 
-    // 🎤 콘서트
-    CONCERT_PERFORMANCE_IMMERSION("🎤 공연 몰입러",        ContentCategory.CONCERT, GroupType.A),
-    CONCERT_SUMMARY_QUEEN("🪩 요약 감상요정",               ContentCategory.CONCERT, GroupType.B);
+    /*  🏀 스포츠  */
+    SPORTS_FULL_SUPPORTER  (ContentCategory.SPORTS,  GroupType.A, "🏃 승부에 인생 건 몰입 응원러"),
+    SPORTS_HIGHLIGHT_RABBIT(ContentCategory.SPORTS,  GroupType.B, "🧊 알잘딱깔센 스포츠러"),
 
-    private final String label;
+    /*  🎤 콘서트  */
+    CONCERT_STAGE_DIVER    (ContentCategory.CONCERT, GroupType.A, "🎤 떼창까지 준비된 '공연 몰입러'"),
+    CONCERT_FANCAM_LOVER   (ContentCategory.CONCERT, GroupType.B, "🪩 레전드 직캠 모으는 요약 감상요정");
+
     private final ContentCategory category;
     private final GroupType group;
+    private final String label;
 
-    UserType(String label, ContentCategory category, GroupType group) {
-        this.label = label;
+    UserType(ContentCategory category, GroupType group, String label) {
         this.category = category;
-        this.group = group;
+        this.group    = group;
+        this.label    = label;
     }
 }
