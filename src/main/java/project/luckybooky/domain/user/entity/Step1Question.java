f0482d0b61f2a0e3c5a77da0ccf00384ae365c41
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-03 14:04:15 +0900

    [FIX] #16 enum 라벨 정보 수정 및 추가

--- src/main/java/project/luckybooky/domain/user/entity/Step1Question.java
+++ src/main/java/project/luckybooky/domain/user/entity/Step1Question.java
@@ -1,5 +1,23 @@
 package project.luckybooky.domain.user.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum Step1Question {
-    STRESSED, REPETITIVE, SEEKING_THRILL, CHATTY, RELAXED, LAZY;
+
+    STRESSED      ("💼", "하루하루가 전쟁이에요."),
+    REPETITIVE    ("🔁", "똑같은 하루의 반복, 좀 지루해요."),
+    SEEKING_THRILL("🌍", "색다른 자극이 그리워요."),
+    CHATTY        ("🗣", "그냥 누군가랑 웃고 수다 떨고 싶어요."),
+    RELAXED       ("☕", "요즘은 한가하고, 마음도 꽤 여유로워요."),
+    LAZY          ("🛋", "아무 생각 없이 쉬고만 싶어요.");
+
+    private final String emoji;
+    private final String answerText;
+
+    Step1Question(String emoji, String answerText) {
+        this.emoji      = emoji;
+        this.answerText = answerText;
+    }
 }
+
