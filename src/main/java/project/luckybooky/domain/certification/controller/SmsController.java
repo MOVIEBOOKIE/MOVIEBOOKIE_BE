c233ac0e3ad8884dbe913faf393648c7dd1b73ee
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-07 03:10:49 +0900

    [FEAT] #18: 인증번호 검증 기능 controller 구현

--- src/main/java/project/luckybooky/domain/certification/controller/SmsController.java
+++ src/main/java/project/luckybooky/domain/certification/controller/SmsController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.luckybooky.domain.certification.Service.SmsService;
 import project.luckybooky.domain.certification.dto.request.SmsRequestDTO;
+import project.luckybooky.domain.certification.dto.request.SmsVerifyRequestDTO;
 import project.luckybooky.global.apiPayload.response.CommonResponse;
 import project.luckybooky.global.apiPayload.response.ResultCode;
 
@@ -15,11 +17,21 @@ import project.luckybooky.global.apiPayload.response.ResultCode;
 @RequiredArgsConstructor
 public class SmsController {
 
-    private final project.luckybooky.domain.certification.service.SmsService smsService;
+    private final SmsService smsService;
 
+    /** 인증번호 발송 **/
     @PostMapping("/send")
     public CommonResponse<Void> sendSms(@Valid @RequestBody SmsRequestDTO dto) {
         smsService.sendCertificationCode(dto);
+        return CommonResponse.ok(ResultCode.OK);
+    }
+
+    /**
+     * 인증번호 검증
+     **/
+    @PostMapping("/verify")
+    public CommonResponse<Void> verifySms(@Valid @RequestBody SmsVerifyRequestDTO dto) {
+        smsService.verifyCode(dto);
         return CommonResponse.ok(ResultCode.OK);
     }
 }
