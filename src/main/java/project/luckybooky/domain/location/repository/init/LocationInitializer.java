1842f1b41d3cc11a11f2cb6b7913f5501d0397d0
m020202 <m020202@naver.com>
2025-05-07 10:15:31 +0900

    [REFACTOR] #20: 영화관 더미데이터 수정

--- src/main/java/project/luckybooky/domain/location/repository/init/LocationInitializer.java
+++ src/main/java/project/luckybooky/domain/location/repository/init/LocationInitializer.java
@@ -5,11 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import project.luckybooky.domain.location.entity.Location;
+import project.luckybooky.domain.location.entity.type.LocationKeyword;
 import project.luckybooky.domain.location.repository.LocationRepository;
 import project.luckybooky.global.util.DummyDataInit;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,24 +27,34 @@ public class LocationInitializer implements ApplicationRunner {
         } else {
             List<Location> locationList = new ArrayList<>();
 
-            Location 롯데시네마_신촌관 = Location.builder()
-                    .locationName("롯데시네마 신촌관")
-                    .address("서울특별시 마포구 양화로 176")
+            HashSet<LocationKeyword> keywords1 = new HashSet<>();
+            keywords1.add(LocationKeyword.LARGE_SCALE);
+            keywords1.add(LocationKeyword.FOR_GROUP);
+            keywords1.add(LocationKeyword.LIVE_FEEL);
+            keywords1.add(LocationKeyword.MAJESTIC);
+            Location location1 = Location.builder()
+                    .locationName("한국영상자료원 시네마파크 KOFA 1관")
+                    .address("서울특별시 마포구 상암동 1602 DMC 단지")
+                    .pricePerHour(40000L)
+                    .locationKeywordList(keywords1)
+                    .hasDisabledSeat(Boolean.TRUE)
                     .build();
 
-            Location 롯데시네마_안양일번가관 = Location.builder()
-                    .locationName("롯데시네마 안양일번가관")
-                    .address("경기도 안양시 만안구 안양로 311")
+            HashSet<LocationKeyword> keywords2 = new HashSet<>();
+            keywords2.add(LocationKeyword.SMALL_SCALE);
+            keywords2.add(LocationKeyword.PREMIUM);
+            keywords2.add(LocationKeyword.FOR_COUPLE);
+            keywords2.add(LocationKeyword.FOR_FAMILY);
+            Location location2 = Location.builder()
+                    .locationName("코엑스 더 부티크 프라이빗 1관")
+                    .address("서울 마포구 상암동 1602 DMC 단지")
+                    .pricePerHour(20000L)
+                    .locationKeywordList(keywords2)
+                    .hasDisabledSeat(Boolean.TRUE)
                     .build();
 
-            Location 롯데시네마_범계관 = Location.builder()
-                    .locationName("롯데시네마 범계관")
-                    .address("경기도 안양시 동안구 시민대로 180")
-                    .build();
-
-            locationList.add(롯데시네마_신촌관);
-            locationList.add(롯데시네마_안양일번가관);
-            locationList.add(롯데시네마_범계관);
+            locationList.add(location1);
+            locationList.add(location2);
 
             locationRepository.saveAll(locationList);
         }
