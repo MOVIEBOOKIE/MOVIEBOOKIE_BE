6ebbdfd82b67b70b4805e6b4a40cc6ba9d39a51d
m020202 <m020202@naver.com>
2025-05-07 11:26:02 +0900

    [FEAT] #20: 이벤트 생성 관련 요청/응답 DTO 추가

--- src/main/java/project/luckybooky/domain/event/controller/EventController.java
+++ src/main/java/project/luckybooky/domain/event/controller/EventController.java
@@ -1,10 +1,15 @@
 package project.luckybooky.domain.event.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import project.luckybooky.domain.event.dto.request.EventRequest;
+import project.luckybooky.domain.event.dto.response.EventResponse;
 import project.luckybooky.domain.event.service.EventService;
+import project.luckybooky.global.apiPayload.common.BaseResponse;
 
 @Tag(name = "Event", description = "이벤트 관련 API")
 @RestController
@@ -12,4 +17,14 @@ import project.luckybooky.domain.event.service.EventService;
 @RequestMapping("/api/event")
 public class EventController {
     private final EventService eventService;
+
+    @Operation(summary = "이벤트 생성", description = "해당하는 값을 넣어주세요 !")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public BaseResponse<EventResponse.EventCreateResultDTO> signUp(
+            @RequestPart EventRequest.EventCreateRequestDTO request,
+            @RequestPart MultipartFile eventImage
+    ) {
+        return BaseResponse.onSuccess(eventService.createEvent(request, eventImage));
+    }
+
 }
