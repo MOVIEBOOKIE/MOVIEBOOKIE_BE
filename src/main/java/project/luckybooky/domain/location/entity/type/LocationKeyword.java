fd944efe79c98fe224dc3bea0db25bc5ab84f7d9
m020202 <m020202@naver.com>
2025-05-10 01:22:42 +0900

    [FEAT] #26: 유형 추가

--- src/main/java/project/luckybooky/domain/location/entity/type/LocationKeyword.java
+++ src/main/java/project/luckybooky/domain/location/entity/type/LocationKeyword.java
@@ -8,10 +8,16 @@ import project.luckybooky.domain.event.entity.type.EventStatus;
 @Getter
 public enum LocationKeyword {
     LARGE_SCALE("대규모"),
+    MEDIUM_SCALE("중규모"),
     SMALL_SCALE("소규모"),
+    NORMAL("적당한"),
+    PRIVATE("프라이빗한"),
+    COZINESS("아늑한"),
+    QUIETNESS("조용한"),
     LIVE_FEEL("현장감"),
     MAJESTIC("웅장감"),
     PREMIUM("프리미엄"),
+    IMMERSION("몰입"),
     FOR_GROUP("단체"),
     FOR_COUPLE("연인"),
     FOR_FAMILY("가족");
