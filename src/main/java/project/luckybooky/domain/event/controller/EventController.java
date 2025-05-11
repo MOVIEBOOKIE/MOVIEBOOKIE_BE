8556b418936ac817b9651570ccb0168d5f8ddd05
m020202 <m020202@naver.com>
2025-05-11 13:29:23 +0900

    [FEAT] #29: 카테고리별 이벤트 목록 조회 API 추가

--- src/main/java/project/luckybooky/domain/event/controller/EventController.java
+++ src/main/java/project/luckybooky/domain/event/controller/EventController.java
@@ -2,9 +2,8 @@ package project.luckybooky.domain.event.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.hibernate.query.Page;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import project.luckybooky.domain.event.dto.request.EventRequest;
@@ -15,6 +14,8 @@ import project.luckybooky.domain.user.dto.response.UserResponseDTO;
 import project.luckybooky.domain.user.service.AuthService;
 import project.luckybooky.domain.user.util.AuthenticatedUserUtils;
 import project.luckybooky.global.apiPayload.common.BaseResponse;
+
+import java.util.List;
 
 @Tag(name = "Event", description = "이벤트 관련 API")
 @RestController
@@ -30,8 +31,7 @@ public class EventController {
     public BaseResponse<EventResponse.EventCreateResultDTO> createEvent(
             @RequestPart EventRequest.EventCreateRequestDTO request,
             @RequestPart(required = false) MultipartFile eventImage
-            ) 
-    {
+    ) {
         // 유저 ID 가져오기
         String userEmail = AuthenticatedUserUtils.getAuthenticatedUserEmail();
         UserResponseDTO.AllInfoDTO userInfo = authService.getUserInfo(userEmail);
@@ -41,4 +41,15 @@ public class EventController {
         return BaseResponse.onSuccess(participationService.createParticipation(userId, eventId, Boolean.TRUE));
     }
 
+    @Operation(summary = "카테고리별 이벤트 리스트 조회", description = "조회를 희망하는 카테고리와 page&size를 넣어주세요!! <br><br>" +
+            "category: 조회할 카테고리 (ex, 인기, 최신, 영화, 드라마, 스포츠, 예능, 콘서트, 그외 이벤트 중 1개) <br>" +
+            "page: 조회할 페이지 번호 <br> size: 한 페이지에 조회할 이벤트 수")
+    @GetMapping("/category")
+    public BaseResponse<List<EventResponse.EventReadByCategoryResultDTO>> readEventListByCategory(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam String category
+    ) {
+        return BaseResponse.onSuccess(eventService.readEventListByCategory(category, page, size));
+    }
 }
