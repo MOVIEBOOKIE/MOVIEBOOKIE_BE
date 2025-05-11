2b4ff60b4e5adbd6b87566943472f5597ac287b0
m020202 <m020202@naver.com>
2025-05-11 22:45:41 +0900

    [FEAT] #33: 영화관->영화관 목록 조회 DTO 변환 메서드 생성

---
+++ src/main/java/project/luckybooky/domain/location/converter/LocationConverter.java
@@ -0,0 +1,19 @@
+package project.luckybooky.domain.location.converter;
+
+import project.luckybooky.domain.event.dto.response.EventResponse;
+import project.luckybooky.domain.location.dto.response.LocationResponse;
+import project.luckybooky.domain.location.entity.Location;
+
+import java.util.Set;
+
+public class LocationConverter {
+    public static LocationResponse.ReadLocationsResultDTO toReadLocationsResultDTO(Location location, Set<String> keywords) {
+        return LocationResponse.ReadLocationsResultDTO.builder()
+                .locationName(location.getLocationName())
+                .locationImageUrl(location.getLocationImageUrl())
+                .locationKeywordList(keywords)
+                .seatCount(location.getSeatCount())
+                .hasDisabledSeat(location.getHasDisabledSeat())
+                .build();
+    }
+}
