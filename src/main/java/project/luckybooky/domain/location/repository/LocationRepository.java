ade9e9992df7bf0ba743fbc0b8340532334106e2
m020202 <m020202@naver.com>
2025-05-11 22:46:49 +0900

    [FEAT] #33: 이벤트 특성에 맞는 영화관 추천 로직 구현

--- src/main/java/project/luckybooky/domain/location/repository/LocationRepository.java
+++ src/main/java/project/luckybooky/domain/location/repository/LocationRepository.java
@@ -1,7 +1,24 @@
 package project.luckybooky.domain.location.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.luckybooky.domain.location.entity.Location;
 
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Query("SELECT l FROM Location l " +
+            "WHERE (:min BETWEEN 1 AND l.seatCount OR :max BETWEEN 1 AND l.seatCount) " +
+            "AND (:mediaType = '기타' OR l.availableMediaType = 'ALL') " +
+            "AND (l.isStartTimeRestricted = FALSE OR " +
+            "    (l.isStartTimeRestricted = TRUE AND :startTime IN (SELECT a FROM l.allowedStartTimes a))) " +
+            "AND (l.availableTimes = 0 OR l.availableTimes = :progressTime)")
+    List<Location> findLocationsByEventOptions(
+            @Param("min") int min,
+            @Param("max") int max,
+            @Param("mediaType") String mediaType,
+            @Param("startTime") String startTime,
+            @Param("progressTime") int progressTime
+    );
 }
