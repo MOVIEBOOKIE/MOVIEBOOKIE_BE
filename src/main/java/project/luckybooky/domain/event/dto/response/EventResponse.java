df3c3fc3cf4408272478e2aa50873450e69a8c45
m020202 <m020202@naver.com>
2025-05-11 13:30:16 +0900

    [FEAT] #29: 카테고리별 이벤트 목록 조회용 응답 DTO 추가

--- src/main/java/project/luckybooky/domain/event/dto/response/EventResponse.java
+++ src/main/java/project/luckybooky/domain/event/dto/response/EventResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class EventResponse {
@@ -17,4 +18,21 @@ public class EventResponse {
         LocalDateTime createdAt;
     }
 
+
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class EventReadByCategoryResultDTO {
+        String mediaType;
+        String mediaTitle;
+        String description;
+        Integer rate;
+        Integer estimatedPrice;
+        LocalDate eventDate;
+        String eventStatus;
+        Integer d_day;
+        String locationName;
+        String posterImageUrl;
+    }
 }
