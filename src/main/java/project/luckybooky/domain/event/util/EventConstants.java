1eac4a139a65a1d0732de607ddba9072dbd15c57
m020202 <m020202@naver.com>
2025-05-13 09:08:36 +0900

    [FEAT] #39: 대관 신청 & 취소 관련 응답 메시지 생성

--- src/main/java/project/luckybooky/domain/event/util/EventConstants.java
+++ src/main/java/project/luckybooky/domain/event/util/EventConstants.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum EventConstants {
     REGISTER_SUCCESS("이벤트 신청이 정상적으로 완료되었습니다."),
     REGISTER_CANCEL_SUCCESS("이벤트 신청 취소가 정상적으로 완료되었습니다."),
+    RECRUIT_CANCEL_SUCCESS("이벤트 모집 취소가 정상적으로 완료되었습니다."),
+    VENUE_RESERVATION_SUCCESS("대관 신청이 정상적으로 완료되었습니다."),
+    VENUE_CANCEL_SUCCESS("대관 취소가 정상적으로 완료되었습니다."),
     ;
 
     private final String message;
