da7f199f5e157f68734c315f3f8da03b05f98415
m020202 <m020202@naver.com>
2025-05-13 23:59:10 +0900

    [FEAT] #45: 상영 확정 혹은 취소 응답 메시지 추가

--- src/main/java/project/luckybooky/domain/event/util/EventConstants.java
+++ src/main/java/project/luckybooky/domain/event/util/EventConstants.java
@@ -12,6 +12,8 @@ public enum EventConstants {
     VENUE_RESERVATION_SUCCESS("대관 신청이 정상적으로 완료되었습니다."),
     VENUE_CANCEL_SUCCESS("대관 취소가 정상적으로 완료되었습니다."),
     VENUE_CONFIRMED("대관 확정이 정상적으로 완료되었습니다."),
+    SCREENING_DONE("상영 완료가 정상적으로 처리되었습니다."),
+    SCREENING_CANCEL_SUCCESS("상영 취소가 정상적으로 처리되었습니다."),
     ;
 
     private final String message;
