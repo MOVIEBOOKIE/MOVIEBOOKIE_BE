d8d5710f8dd31f438b796a88f35cda72a6018e13
m020202 <m020202@naver.com>
2025-05-15 10:50:19 +0900

    [FEAT] #51: 티켓 엔티티 <-> DTO 변환 컨버터 생성

--- src/main/java/project/luckybooky/domain/ticket/converter/TicketConverter.java
+++ src/main/java/project/luckybooky/domain/ticket/converter/TicketConverter.java
@@ -1,6 +1,7 @@
 package project.luckybooky.domain.ticket.converter;
 
 import project.luckybooky.domain.event.entity.Event;
+import project.luckybooky.domain.ticket.dto.response.TicketResponse;
 import project.luckybooky.domain.ticket.entity.Ticket;
 import project.luckybooky.domain.user.entity.User;
 
@@ -20,6 +21,21 @@ public class TicketConverter {
                 .eventImageUrl(event.getPosterImageUrl())
                 .participants(event.getCurrentParticipants())
                 .userList(userList)
+                .build();
+    }
+
+    public static TicketResponse.ReadTicketDetailsResultDTO toReadTicketDetailsResultDTO(Ticket ticket) {
+        return TicketResponse.ReadTicketDetailsResultDTO.builder()
+                .ticketId(ticket.getId())
+                .title(ticket.getTitle())
+                .type(ticket.getType())
+                .location(ticket.getLocation())
+                .price(ticket.getPrice())
+                .hostName(ticket.getHostName())
+                .participants(ticket.getParticipants())
+                .time(ticket.getTime())
+                .scheduledAt(ticket.getScheduledAt())
+                .address(ticket.getAddress())
                 .build();
     }
 }
