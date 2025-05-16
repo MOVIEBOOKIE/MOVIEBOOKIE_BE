ab75e7c7293400b5d05cdc874588d038be1c78d4
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-16 16:37:39 +0900

    [FEAT] #54: 마이페이지 api 구현

---
+++ src/main/java/project/luckybooky/domain/user/controller/MypageController.java
@@ -0,0 +1,26 @@
+package project.luckybooky.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.luckybooky.domain.user.dto.response.MypageResponseDTO;
+import project.luckybooky.domain.user.service.MypageService;
+import project.luckybooky.global.apiPayload.response.CommonResponse;
+import project.luckybooky.global.apiPayload.response.ResultCode;
+
+@RestController
+@RequestMapping("/api/mypage")
+@RequiredArgsConstructor
+public class MypageController {
+
+    private final MypageService mypageService;
+
+    @Operation(summary = "마이페이지 기본 정보 조회")
+    @GetMapping
+    public CommonResponse<MypageResponseDTO> getMypage() {
+        MypageResponseDTO dto = mypageService.getMyPage();
+        return CommonResponse.of(ResultCode.OK, dto);
+    }
+}
