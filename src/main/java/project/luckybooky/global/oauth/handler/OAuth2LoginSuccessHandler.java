d248a514e28709c8a9481e109563ae91fd72f13c
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-17 19:39:25 +0900

    [CHORE] 배포 도메인 이름 수정

--- src/main/java/project/luckybooky/global/oauth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/project/luckybooky/global/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -32,7 +32,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         CookieUtil.addCookie(response, "accessToken", accessToken, jwtUtil.getAccessTokenValidity(), isLocal);
         CookieUtil.addCookie(response, "refreshToken", refreshToken, jwtUtil.getRefreshTokenValidity(), isLocal);
 
-        String redirectUrl = isLocal ? "http://localhost:3000" : "https://moviebookie-develop.vercel.app/login/kakao";
+        String redirectUrl = isLocal ? "http://localhost:3000" : "https://movie-bookie.shop/login/kakao";
         log.info("🔹 로그인 성공! {} 환경으로 리디렉트: {}", isLocal ? "로컬" : "배포", redirectUrl);
 
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
