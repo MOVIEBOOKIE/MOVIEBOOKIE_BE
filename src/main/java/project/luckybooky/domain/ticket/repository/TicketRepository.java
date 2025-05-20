e8591c4e604f7e880fa39c66e7bd486377e448d7
m020202 <m020202@naver.com>
2025-05-20 11:55:51 +0900

    [FEAT] #72: 티켓 목록 조회 로직 구현

--- src/main/java/project/luckybooky/domain/ticket/repository/TicketRepository.java
+++ src/main/java/project/luckybooky/domain/ticket/repository/TicketRepository.java
@@ -1,7 +1,13 @@
 package project.luckybooky.domain.ticket.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.luckybooky.domain.ticket.entity.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("SELECT t FROM Ticket t JOIN t.userList u WHERE u.id = :userId")
+    Page<Ticket> findTicketsByUserId(@Param("userId") Long userId, Pageable pageable);
 }
