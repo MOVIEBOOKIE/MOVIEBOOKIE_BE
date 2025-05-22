ad375390cba663ddcd46ee4c1e5929f0f9ea4377
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-22 16:53:59 +0900

    [CHORE] #77: mailsender 수동 빈 등록으로 수정

---
+++ src/main/java/project/luckybooky/global/config/MailConfig.java
@@ -0,0 +1,35 @@
+package project.luckybooky.global.config;
+
+import java.util.Properties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+@Getter
+@Setter
+public class MailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    private Properties properties;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(host);
+        sender.setPort(port);
+        sender.setUsername(username);
+        sender.setPassword(password);
+        sender.setJavaMailProperties(properties);
+        return sender;
+    }
+}
