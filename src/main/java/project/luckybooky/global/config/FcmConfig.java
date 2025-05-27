a57ecafddff6bd28455496a1e7d4ea6cc78f32b2
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-05-27 15:13:16 +0900

    [CHORE] #85: fcm 클래스패스 리소스로 로딩하도록 수정

--- src/main/java/project/luckybooky/global/config/FcmConfig.java
+++ src/main/java/project/luckybooky/global/config/FcmConfig.java
@@ -1,38 +1,39 @@
-//package project.luckybooky.global.config;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import jakarta.annotation.PostConstruct;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
-//import project.luckybooky.global.apiPayload.error.dto.ErrorCode;
-//import project.luckybooky.global.apiPayload.error.exception.BusinessException;
-//
-//@Configuration
-//@Slf4j
-//public class FcmConfig {
-//
-//    @PostConstruct
-//    public void initialize() {
-//        try {
-//            if (FirebaseApp.getApps().isEmpty()) {
+package project.luckybooky.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import project.luckybooky.global.apiPayload.error.dto.ErrorCode;
+import project.luckybooky.global.apiPayload.error.exception.BusinessException;
+
+@Configuration
+@Slf4j
+public class FcmConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
 //                FileInputStream serviceAccount =
 //                        new FileInputStream(
 //                                "src/main/resources/secret/moviebooky-2009d-firebase-adminsdk-fbsvc-1527730d9b.json");
-//
-//                FirebaseOptions options = FirebaseOptions.builder()
-//                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                        .build();
-//
-//                FirebaseApp.initializeApp(options);
-//                log.info("✅ FirebaseApp 초기화 완료");
-//            }
-//        } catch (IOException e) {
-//            log.error("❌ FirebaseApp 초기화 실패", e);
-//            throw new BusinessException(ErrorCode.FCM_INITIALIZATION_FAILED);
-//        }
-//    }
-//}
+                ClassPathResource resource = new ClassPathResource(
+                        "secret/moviebooky-2009d-firebase-adminsdk-fbsvc-1527730d9b.json");
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+                log.info("✅ FirebaseApp 초기화 완료");
+            }
+        } catch (IOException e) {
+            log.error("❌ FirebaseApp 초기화 실패", e);
+            throw new BusinessException(ErrorCode.FCM_INITIALIZATION_FAILED);
+        }
+    }
+}
