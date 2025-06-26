a5f21f79863576e1df6ace91e073cc25bf195e5e
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-06-26 22:25:59 +0900

    [FEAT] #146: 비동기 메일 전송 처리 활성화

---
+++ src/main/java/project/luckybooky/global/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package project.luckybooky.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean("mailExecutor")
+    public TaskExecutor mailExecutor() {
+        ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
+        exec.setCorePoolSize(10);
+        exec.setMaxPoolSize(20);
+        exec.setQueueCapacity(100);
+        exec.setThreadNamePrefix("mail-exec-");
+        exec.initialize();
+        return exec;
+    }
+}
+
