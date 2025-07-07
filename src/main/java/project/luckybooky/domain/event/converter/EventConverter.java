75006051395202e0918500dade252f5f9fe267df
m020202 <m020202@naver.com>
2025-07-07 18:48:53 +0900

    [REFACTOR] #159: 불필요한 import 제거

--- src/main/java/project/luckybooky/domain/event/converter/EventConverter.java
+++ src/main/java/project/luckybooky/domain/event/converter/EventConverter.java
@@ -8,7 +8,6 @@ import project.luckybooky.domain.location.entity.Location;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
