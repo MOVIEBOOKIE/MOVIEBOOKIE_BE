886f056e4ca770eacca1c264ad52f621d455b2ac
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-07-08 22:10:44 +0900

    [FEAT] #162 알림 엔티티, 레포지토리 설정

---
+++ src/main/java/project/luckybooky/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package project.luckybooky.domain.notification.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.luckybooky.domain.notification.entity.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findAllBySender(Long userId);
+}
