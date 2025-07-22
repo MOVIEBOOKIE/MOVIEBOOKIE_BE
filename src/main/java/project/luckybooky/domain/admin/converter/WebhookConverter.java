78ee87effb70989b838c7cdef847fa0836281e60
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-07-23 06:58:06 +0900

    [FEAT] #164: 대관 확정 메일 전송 웹훅 기능 구현

--- src/main/java/project/luckybooky/domain/admin/converter/WebhookConverter.java
+++ src/main/java/project/luckybooky/domain/admin/converter/WebhookConverter.java
@@ -1,8 +1,5 @@
 package project.luckybooky.domain.admin.converter;
 
-import static project.luckybooky.domain.participation.entity.type.ParticipateRole.HOST;
-import static project.luckybooky.domain.participation.entity.type.ParticipateRole.PARTICIPANT;
-
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import project.luckybooky.domain.admin.dto.ParticipantInfo;
@@ -13,43 +10,46 @@ import project.luckybooky.global.apiPayload.error.dto.ErrorCode;
 import project.luckybooky.global.apiPayload.error.exception.BusinessException;
 
 public class WebhookConverter {
-
-    public static VenueRequestWebhookDTO toDto(Event event, List<Participation> participations) {
+    
+    public static VenueRequestWebhookDTO toDto(
+            Event event,
+            Participation hostParticipation,
+            List<Participation> participantList
+    ) {
         // 날짜, 시간, 장소
         String date = event.getEventDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
         String time = event.getEventStartTime() + "-" + event.getEventEndTime();
         String location = event.getLocation().getLocationName();
 
-        // 호스트 찾기 (없으면 예외)
-        Participation hostParticipation = participations.stream()
-                .filter(p -> p.getParticipateRole() == HOST)
-                .findFirst()
-                .orElseThrow(() -> new BusinessException(ErrorCode.PARTICIPATION_NOT_FOUND));
-        project.luckybooky.domain.user.entity.User hostUser = hostParticipation.getUser();
+        // 호스트 정보 검증
+        if (hostParticipation == null) {
+            throw new BusinessException(ErrorCode.PARTICIPATION_NOT_FOUND);
+        }
+        var hostUser = hostParticipation.getUser();
+        String hostName = hostUser.getUsername();
+        String hostPhone = hostUser.getPhoneNumber();
 
         // 참여자 정보 리스트
-        List<ParticipantInfo> participantInfos = participations.stream()
-                .filter(p -> p.getParticipateRole() == PARTICIPANT)
+        List<ParticipantInfo> participantInfos = participantList.stream()
                 .map(p -> {
-                    project.luckybooky.domain.user.entity.User u = p.getUser();
+                    var u = p.getUser();
                     return new ParticipantInfo(
                             u.getCertificationEmail(),
                             u.getUsername(),
                             u.getPhoneNumber(),
-                            u.getUserType().getTitle()  // UserType의 title 필드
+                            u.getUserType().getTitle()
                     );
                 })
                 .toList();
 
-        // 목적(purpose) 필드: 이벤트 설명을 사용
         String purpose = event.getDescription();
 
         return new VenueRequestWebhookDTO(
                 date,
                 time,
                 location,
-                hostUser.getUsername(),
-                hostUser.getPhoneNumber(),
+                hostName,
+                hostPhone,
                 participantInfos.size(),
                 purpose,
                 participantInfos
