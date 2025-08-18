2a0195c3e0b44b0f1de75c91a4ee3f8e76aa1614
gwanghyeon-k <gwanghyeonkim01@gmail.com>
2025-08-19 02:55:07 +0900

    [CHORE] #218: 시큐리티 엔드포인트 중복 허용 수정

--- src/main/java/project/luckybooky/global/config/SecurityConfig.java
+++ src/main/java/project/luckybooky/global/config/SecurityConfig.java
@@ -62,15 +62,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/events/anonymous/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/email/send").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/test/users/tokens/**").permitAll()
-                        .requestMatchers("/", "/api/health").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/email/send").permitAll()
+                        .requestMatchers("/api/health").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/events/anonymous/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/auth/login/kakao").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/reissue").permitAll()
                         .requestMatchers("/events/*/participants", "/events/*/participants/link").permitAll()
                         .requestMatchers(
-                                "index.html",
+                                "/index.html",
                                 "/favicon.ico",
                                 "/css/**",
                                 "/js/**",
