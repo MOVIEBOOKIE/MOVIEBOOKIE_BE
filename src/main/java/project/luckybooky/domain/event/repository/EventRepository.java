05de785652fddcd12148d59b5bedebe2f8650a93
m020202 <m020202@naver.com>
2025-10-20 15:09:54 +0900

    [REFACTOR] #254: 이벤트 목록 조회 시 공개된 이벤트만 조회되도록 수정

--- src/main/java/project/luckybooky/domain/event/repository/EventRepository.java
+++ src/main/java/project/luckybooky/domain/event/repository/EventRepository.java
@@ -13,25 +13,25 @@ import org.springframework.data.repository.query.Param;
 import project.luckybooky.domain.event.entity.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-    @Query("SELECT e FROM Event e WHERE e.eventStatus='RECRUITING' ORDER BY e.createdAt DESC")
+    @Query("SELECT e FROM Event e WHERE e.isPublic = true AND e.eventStatus='RECRUITING' ORDER BY e.createdAt DESC")
     Page<Event> findOrderByCreatedAt(Pageable pageable);
 
-    @Query("SELECT e FROM Event e JOIN e.category c WHERE c.categoryName = :categoryName AND e.eventStatus='RECRUITING' ORDER BY e.recruitmentEnd")
+    @Query("SELECT e FROM Event e JOIN e.category c WHERE e.isPublic = true AND c.categoryName = :categoryName AND e.eventStatus='RECRUITING' ORDER BY e.recruitmentEnd")
     Page<Event> findByCategoryName(@Param("categoryName") String categoryName, Pageable pageable);
 
-    @Query("SELECT e FROM Event e WHERE e.eventStatus='RECRUITING' ORDER BY (e.currentParticipants * 1.0) / e.maxParticipants DESC")
+    @Query("SELECT e FROM Event e WHERE e.isPublic = true AND e.eventStatus='RECRUITING' ORDER BY (e.currentParticipants * 1.0) / e.maxParticipants DESC")
     Page<Event> findOrderByPopularity(Pageable pageable);
 
     @Query("SELECT e FROM Event e WHERE e.recruitmentEnd < :now and e.eventStatus='RECRUITING'")
     List<Event> findExpiredEvent(@Param("now") LocalDate now);
 
     @Query("SELECT e FROM Event e \n" +
-            "WHERE :content IN (e.category.categoryName) \n" +
+            "WHERE e.isPublic = true AND :content IN (e.category.categoryName) \n" +
             "   OR e.mediaTitle LIKE CONCAT('%', :content, '%')")
     Page<Event> findEventsBySearch(@Param("content") String content, Pageable pageable);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE e.category.id = :categoryId " +
+            "WHERE e.isPublic = true AND e.category.id = :categoryId " +
             "AND e.eventStatus = 'RECRUITING'" +
             "ORDER BY (1.0 * e.currentParticipants / e.maxParticipants) DESC")
     List<Event> findEventListByUserType(@Param("categoryId") Long categoryId, Pageable pageable);
